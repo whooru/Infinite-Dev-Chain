ce6ca763d5a8133c6f67977ed5d826a0d949a217
Vlad Longer <whooru13@gmail.com>
2025-08-18 01:14:17 +0700

    feat(idc-application): add DTOs, query, and service API skeleton; add example test

---
+++ languages/kotlin/idc/src/test/kotlin/idc/application/DescribeElementsServiceTest.kt
@@ -0,0 +1,11 @@
+package idc.application
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class DescribeElementsServiceTest {
+    @Test
+    fun placeholder() {
+        assertTrue(true)
+    }
+}
